bc8b8edee011124bf46e14bd44f7120cc135ed97
fltenwall <48324481+fltenwall@users.noreply.github.com>
2021-10-05 20:38:01 +0800

    Q6:字符串和文本

---
+++ Q6-字符串和文本/code6-1.py
@@ -0,0 +1,5 @@
+types_of_people = 10
+print(f'the number is {types_of_people}') # the number is 10
+
+str = 'the number is {}'
+print(str.format(types_of_people)) # the number is 10
---
+++ Q6-字符串和文本/code6-2.py
@@ -0,0 +1,7 @@
+types_of_people = 10
+print(f'the number is {types_of_people} or not') # the number is 10 or not
+
+num2 = 20
+num3 = 30
+str = 'the number is {}, or {}, or {}.'
+print(str.format(types_of_people, num2, num3)) # the number is 10, or 20, or 30.
